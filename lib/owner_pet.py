436f328cb148e08e2ba063ec1940ee67a0e44ee3
Meaghan <meaghan.sharrard89@gmail.com>
2023-12-13 12:59:29 -0800

    passing tests

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,45 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Owner.all.append(self)
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Invalid pet type.")
+
+        if pet.pet_type not in Owner.PET_TYPES:
+            raise Exception("Invalid pet type.")
+
+        pet.owner = self
+        self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+
+class Pet(Owner):
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        super().__init__(name)
+        self.pet_type = pet_type
+        self.owner = owner
+        self.validate_pet_type(pet_type)
+        Pet.add_pet(self)
+        if owner:
+            owner.add_pet(self)
+
+    def validate_pet_type(self, pet_type):
+        if pet_type not in Owner.PET_TYPES:
+            raise Exception(f"Invalid pet type.")
+
+    @classmethod
+    def add_pet(cls, pet):
+        cls.all.append(pet)
